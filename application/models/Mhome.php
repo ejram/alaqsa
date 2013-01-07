bdc27fd9680a2115afa6cf19849b10843fb73a0b
marwanzak <marwan_zk@yahoo.com>
2013-01-06 23:39:20 -0800

    new commit

--- application/models/Mhome.php
+++ application/models/Mhome.php
@@ -10,22 +10,29 @@
        public function Print_query($table) {
   
         
-        $tmpl = array ( 'table_open'  => '<table border="1" cellpadding="2" 
-                                           cellspacing="1" class="mytable" dir="rtl">' 
+        $tmpl = array ( 'table_open'  => '<table border="1"
+										  cellpadding="2" 
+                                          cellspacing="1"
+        								  class="mytable" 
+        								  dir="rtl">' 
                       );
-        $this->table->set_template($tmpl); 
-        $this->table->set_heading('المرحلة', 'الصف',
+        $this -> table -> set_template($tmpl); 
+        $this -> table -> set_heading('المرحلة', 'الصف',
                                   'مجموع الفصول', 'إضافة فصل',
                                   'تعديل فصل','مسح الصف'
                                  );
         
-        $Q=$this->Get_query_all($table);
-        foreach ($Q-> result() as $row)
+        $Q = $this -> Get_query_all($table);
+        foreach ($Q -> result() as $row)
     {
         $form_att = array('id' => 'table_form');
         echo form_open('/most',$form_att); 
-        $this->table->add_row($row->aqsa_level,$row->aqsa_class,$row->class_dep_num,
-         anchor('#', '+'),'تعديل','<span id=\'delete\' onClick=\' \'>delete</span>');
+        $this -> table -> add_row($row->aqsa_level,
+        						  $row->aqsa_class,
+        						  $row->class_dep_num,
+         						  anchor('#', '+'), 'تعديل',
+        						  '<span id=\'delete\' 
+        						  onClick=\' \'>delete</span>');
 
     }            
     
