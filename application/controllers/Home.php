003f936f5a8764044450e2fd223944ff538acba9
marwanzak <marwan_zk@yahoo.com>
2013-01-30 17:33:23 +0200

    1/30 from home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,9 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->check_isvalidated();
+		echo 'Congratulations, you are logged in.';
+		echo '<br /><a href='.base_url().'home/do_logout>Logout Fool!</a>';
+	}
 
 	public function index()
 	{
+
 		$this->c_panel('aq_levels');
 	}
 	public function c_panel($table_data)
@@ -13,6 +20,15 @@ class Home extends CI_Controller {
 		$this->load->view('home_content', $data);
 		$this->load->view('home_footer');
 		$this->load->view('home_addation');
+	}
+	private function check_isvalidated(){
+		if(! $this->session->userdata('validated')){
+			redirect('login');
+		}
+	}
+	public function do_logout(){
+		$this->session->sess_destroy();
+		redirect('login');
 	}
 
 	public function del_query($table, $where, $value)
--- application/models/Mhome.php
+++ application/models/Mhome.php
@@ -75,6 +75,36 @@ class Mhome extends CI_Model {
 		return $rooms;
 			
 	}
+	public function validate(){
+		// grab user input
+		$username = $this->security->xss_clean($this->input->post('username'));
+		$password = $this->security->xss_clean($this->input->post('password'));
+	
+		// Prep the query
+		$this->db->where('user_username', $username);
+		$this->db->where('user_password', $password);
+	
+		// Run the query
+		$query = $this->db->get('aq_users');
+		// Let's check if there are any results
+		if($query->num_rows == 1)
+		{
+			// If there is a user, then create session data
+			$row = $query->row();
+			$data = array(
+					'user_id' => $row->user_id,
+					'user_name' => $row->user_name,
+					'user_username' => $row->user_username,
+					'user_role' => $row->user_role,
+					'validated' => true
+			);
+			$this->session->set_userdata($data);
+			return true;
+		}
+		// If the previous process did not validate
+		// then return false.
+		return false;
+	}
 	
 
 }
